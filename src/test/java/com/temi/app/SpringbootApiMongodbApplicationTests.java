06e695a3f25584aa9ac1e83eaf3e111fce660c45
temitopejegede <temiteejegede@gmail.com>
2022-07-18 23:53:23 -0400

    Initial commit

---
+++ src/test/java/com/temi/app/SpringbootApiMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.temi.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApiMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

eac3fc7645b9d7ea8ee78a2e7de4af2316aacf72
temitopejegede <temiteejegede@gmail.com>
2022-07-19 22:24:15 -0400

    added constructor that excludes id b/c it is auto generated

--- src/main/java/com/temi/app/Student.java
+++ src/main/java/com/temi/app/Student.java
@@ -2,9 +2,11 @@ package com.temi.app;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.util.List;
 
@@ -13,12 +15,32 @@ import java.util.List;
 public class Student {
     @Id
     private String id;
+
+    public Student(String firstName,
+                   String lastName,
+                   String email,
+                   Gender gender,
+                   Address address,
+                   List<String> favoriteSubjects,
+                   BigDecimal totalSpentInBooks,
+                   LocalDateTime created) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+        this.address = address;
+        this.favoriteSubjects = favoriteSubjects;
+        this.totalSpentInBooks = totalSpentInBooks;
+        this.created = created;
+    }
+
     private String firstName;
     private String lastName;
+    @Indexed(unique = true)
     private String email;
     private Gender gender;
     private Address address;
     private List<String> favoriteSubjects;
     private BigDecimal totalSpentInBooks;
-    private ZonedDateTime created;
+    private LocalDateTime created;
 }

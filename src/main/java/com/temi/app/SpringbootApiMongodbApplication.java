f6c5b53af20950f9199d0af916e4739bf5cd1dc5
temitopejegede <temiteejegede@gmail.com>
2022-07-19 22:27:04 -0400

    injected commandlinerunner

--- src/main/java/com/temi/app/SpringbootApiMongodbApplication.java
+++ src/main/java/com/temi/app/SpringbootApiMongodbApplication.java
@@ -1,7 +1,14 @@
 package com.temi.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringbootApiMongodbApplication {
@@ -10,4 +17,25 @@ public class SpringbootApiMongodbApplication {
 		SpringApplication.run(SpringbootApiMongodbApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner(StudentRepository repository){
+		return  args -> {
+			Address address = new Address(
+					"England",
+					"London",
+					"NE9"
+			);
+			Student student = new Student(
+					"temi",
+					"jegede",
+					"temi@temi.com",
+					Gender.MALE,
+					address,
+					List.of("Computer Science", "mathematics"),
+					BigDecimal.TEN,
+					LocalDateTime.now()
+			);
+			repository.insert(student);
+		};
+	}
 }

ccba580687a0d10466c3c80837b6ca5cc0fc32fc
temitopejegede <temiteejegede@gmail.com>
2022-07-22 23:48:49 -0400

    added a query using mongo template

--- src/main/java/com/temi/app/SpringbootApiMongodbApplication.java
+++ src/main/java/com/temi/app/SpringbootApiMongodbApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -18,24 +21,39 @@ public class SpringbootApiMongodbApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(StudentRepository repository){
+	CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate){
 		return  args -> {
 			Address address = new Address(
-					"England",
-					"London",
-					"NE9"
+					"Nigeria",
+					"Ikorodu",
+					"098E"
 			);
+			String email = "yusf@temi.com";
 			Student student = new Student(
-					"temi",
-					"jegede",
-					"temi@temi.com",
-					Gender.MALE,
+					"josephine",
+					"yusuf",
+					email,
+					Gender.FEMALE,
 					address,
 					List.of("Computer Science", "mathematics"),
 					BigDecimal.TEN,
 					LocalDateTime.now()
 			);
-			repository.insert(student);
+			Query query = new Query();
+			query.addCriteria(Criteria.where("email").is(email));
+
+			List<Student> students = mongoTemplate.find(query, Student.class);
+
+			if(students.size() > 1){
+				throw new IllegalStateException(
+						"found many students with email: " + email);
+			}
+			if(students.isEmpty()){
+				System.out.println("Inserting student "+ student);
+				repository.insert(student);
+			} else {
+				System.out.println(student + " already exists");
+			}
 		};
 	}
 }
